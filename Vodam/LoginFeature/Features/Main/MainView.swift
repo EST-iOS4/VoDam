90ebbf641934dc231ca7befab42680d53a62d4c7
Song Young Min <iasdsr1347@naver.com>
2025-11-17 21:22:17 +0900

    [REFACTOR] 전체 코드 TCA 최신 문법으로 수정

--- Vodam/LoginFeature/Features/Main/MainView.swift
+++ Vodam/LoginFeature/Features/Main/MainView.swift
@@ -9,54 +9,47 @@ import ComposableArchitecture
 import SwiftUI
 
 struct MainView: View {
-    let store: StoreOf<MainFeature>
+    let store: StoreOf<MainFeature>  // MacinFeature.Action
 
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            VStack {
-                Spacer()
+        VStack {
 
-                Text("여기에 메인 UI 들어갈 예정")
-                    .font(.title3)
-                    .foregroundStyle(.secondary)
+            Text("여기에 메인 UI 들어갈 예정")
+                .font(.title3)
+                .foregroundStyle(.secondary)
 
-                Spacer()
-            }
-            .navigationTitle("새 프로젝트 생성")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        viewStore.send(.profileButtonTapped)
-                    } label: {
-                        Image(systemName: "person.circle")
-                            .imageScale(.large)
-                    }
+        }
+        .navigationTitle("새 프로젝트 생성")
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    store.send(.profileButtonTapped)
+                } label: {
+                    Image(systemName: "person.circle")
+                        .imageScale(.large)
                 }
             }
-            .navigationDestination(
-                isPresented: viewStore.binding(
-                    get: \.isLoginProvidersActive,
-                    send: MainFeature.Action.loginProvidersActiveChanged
-                )
-            ) {
-                LoginProvidersView(
-                    store: store.scope(
-                        state: \.loginProviders,
-                        action: \.loginProviders
-                    )
-                )
-            }
-            // 로그인 유도 sheet
-            .sheet(
-                store: store.scope(
-                    state: \.$profileFlow,
-                    action: \.profileFlow
-                )
-            ) { profileStore in
-                ProfileFlowView(store: profileStore)
-                    .presentationDetents([.fraction(0.4)])
-                    .presentationDragIndicator(.visible)
-            }
+        }
+
+        .navigationDestination(
+            store: store.scope(
+                state: \.$loginProviders,
+                action: \.loginProviders
+            )
+        ) {
+            loginProvidersStore in
+            LoginProvidersView(store: loginProvidersStore)
+        }
+        // 로그인 유도 sheet
+        .sheet(
+            store: store.scope(
+                state: \.$profileFlow,
+                action: \.profileFlow
+            )
+        ) { profileStore in
+            ProfileFlowView(store: profileStore)
+                .presentationDetents([.fraction(0.4)])
+                .presentationDragIndicator(.visible)
         }
     }
 }
--- Vodam/LoginFeature/Features/ProfileFlow/ProfileFlowView.swift
+++ Vodam/LoginFeature/Features/ProfileFlow/ProfileFlowView.swift
@@ -5,23 +5,20 @@
 //  Created by 송영민 on 11/17/25.
 //
 
-
-import SwiftUI
 import ComposableArchitecture
+import SwiftUI
 
 struct ProfileFlowView: View {
     let store: StoreOf<ProfileFlowFeature>
-    
+
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { viewStore in
-            LoginInfoView (
-                onLoginButtonTapped: {
-                    viewStore.send(.loginButtonTapped)
-                },
-                onCancelButtonTapped: {
-                    viewStore.send(.cancelButtonTapped)
-                }
-            )
-        }
+        LoginInfoView(
+            onLoginButtonTapped: {
+                store.send(.loginButtonTapped)
+            },
+            onCancelButtonTapped: {
+                store.send(.cancelButtonTapped)
+            }
+        )
     }
 }
