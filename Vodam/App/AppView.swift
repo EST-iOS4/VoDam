de7acdb7c4692c68bb508117ea7a2ef502433555
Song Young Min <iasdsr1347@naver.com>
2025-11-17 13:03:25 +0900

    feat: 프로필 sheet에 로그인 유도 화면 추가

--- Vodam/App/AppView.swift
+++ Vodam/App/AppView.swift
@@ -12,10 +12,12 @@ struct AppView: View {
     let store: StoreOf<AppFeature>
 
     var body: some View {
-        WithViewStore(store, observe: { $0 }) { _ in
-            MainView(
-                store: store.scope(state: \.main, action: \.main)
-            )
+        NavigationStack{
+            WithViewStore(store, observe: { $0 }) { _ in
+                MainView(
+                    store: store.scope(state: \.main, action: \.main)
+                )
+            }
         }
     }
 }
--- Vodam/Features/ProfileFlow/ProfileFlowFeature.swift
+++ Vodam/Features/ProfileFlow/ProfileFlowFeature.swift
@@ -5,4 +5,23 @@
 //  Created by 송영민 on 11/17/25.
 //
 
-import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct ProfileFlowFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        // 나중에 넣을 예정
+    }
+    
+    enum Action: Equatable {
+        // 나중에 로그인 액션 넣을 예정
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+                .none
+        }
+    }
+}
