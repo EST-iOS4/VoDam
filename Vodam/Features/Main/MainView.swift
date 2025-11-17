63f587865e2c779bdd2b876ece514c5b48a4d9c1
Song Young Min <iasdsr1347@naver.com>
2025-11-17 14:02:42 +0900

    feat: LoginProvidersFeature 추가 및 LoginProvidersView를 구현

--- Vodam/Features/Main/MainView.swift
+++ Vodam/Features/Main/MainView.swift
@@ -5,43 +5,57 @@
 //  Created by 송영민 on 11/17/25.
 //
 
-import SwiftUI
 import ComposableArchitecture
+import SwiftUI
 
 struct MainView: View {
     let store: StoreOf<MainFeature>
-    
+
     var body: some View {
         WithViewStore(store, observe: { $0 }) { viewStore in
-                VStack {
-                    Spacer()
-                    
-                    Text("여기에 메인 UI 들어갈 예정")
-                        .font(.title3)
-                        .foregroundStyle(.secondary)
-                    
-                    Spacer()
-                }
-                .navigationTitle("새 프로젝트 생성")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button{
-                            viewStore.send(.profileButtonTapped)
-                        } label: {
-                            Image(systemName: "person.circle")
-                                .imageScale(.large)
-                        }
+            VStack {
+                Spacer()
+
+                Text("여기에 메인 UI 들어갈 예정")
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+
+                Spacer()
+            }
+            .navigationTitle("새 프로젝트 생성")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        viewStore.send(.profileButtonTapped)
+                    } label: {
+                        Image(systemName: "person.circle")
+                            .imageScale(.large)
                     }
                 }
-                .sheet(
+            }
+            .navigationDestination(
+                isPresented: viewStore.binding(
+                    get: \.isLoginProvidersActive,
+                    send: MainFeature.Action.loginProvidersActiveChanged
+                )
+            ) {
+                LoginProvidersView(
                     store: store.scope(
-                        state: \.$profileFlow,
-                        action: \.profileFlow
+                        state: \.loginProviders,
+                        action: \.loginProviders
                     )
-                ) { profileStore in
-                    ProfileFlowView(store: profileStore)
-                        .presentationDetents([.fraction(0.4)])
-                        .presentationDragIndicator(.visible)
+                )
+            }
+            // 로그인 유도 sheet
+            .sheet(
+                store: store.scope(
+                    state: \.$profileFlow,
+                    action: \.profileFlow
+                )
+            ) { profileStore in
+                ProfileFlowView(store: profileStore)
+                    .presentationDetents([.fraction(0.4)])
+                    .presentationDragIndicator(.visible)
             }
         }
     }
