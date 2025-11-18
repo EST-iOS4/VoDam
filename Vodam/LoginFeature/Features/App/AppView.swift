d80adeaf5e02812d8e1106cc7c6d2c469e9f25bc
Song Young Min <iasdsr1347@naver.com>
2025-11-18 14:18:31 +0900

    [MOVE] 파일 구조 변경 + [DEL] 쓸모없는 파일 삭제

--- Vodam/LoginFeature/Features/App/AppView.swift
+++ Vodam/LoginFeature/Features/App/AppView.swift
@@ -9,11 +9,13 @@ import ComposableArchitecture
 import SwiftUI
 
 struct AppView: View {
+    // 모든 곳에서 일아는 상태나 리듀서를 갖고있어요)
     let store: StoreOf<AppFeature>
 
     var body: some View {
         NavigationStack{
                 MainView(
+                    //키를 통해서
                    store: store.scope(state: \.main, action: \.main)
                 )
             }
