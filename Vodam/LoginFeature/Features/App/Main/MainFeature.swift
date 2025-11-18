12b26d499e1fc9bd5e91726d35e53d0dd3fe8ff3
Song Young Min <iasdsr1347@naver.com>
2025-11-18 14:18:31 +0900

    [MOVE] 파일 구조 변경 + [DEL] 쓸모없는 파일 삭제

--- Vodam/LoginFeature/Features/App/Main/MainFeature.swift
+++ Vodam/LoginFeature/Features/App/Main/MainFeature.swift
@@ -12,7 +12,10 @@ struct MainFeature {
     
     @ObservableState
     struct State: Equatable {
-        @Presents var profileFlow: ProfileFlowFeature.State?
+        
+        //Presents SwiftUI의 @State 변수가
+        @Presents var profileFlow: ProfileFlowFeature.State? = nil
+        
     
         @Presents var loginProviders: LoginProvidersFeature.State?
     }
@@ -24,6 +27,15 @@ struct MainFeature {
         case loginProviders(PresentationAction<LoginProvidersFeature.Action>)
         
         case dismissProfileSheet
+        
+//        case delegate(PresentationAction<ProfileFlowFeature.Action>)
+        case delegate(Delegate)
+        
+        enum Delegate: Equatable {
+            case shouldNavigateToLogin
+            case didOpenProfileSheet
+            case loginButtonTapped
+        }
     }
     
     var body: some Reducer<State, Action> {
@@ -34,12 +46,20 @@ struct MainFeature {
                 state.profileFlow = ProfileFlowFeature.State()
                 return .none
                 
+                
+//            case .profileFlow(.presented(.loginButtonTapped)):
+//                // 1) 로그인 안내 시트 닫기
+//                
+//                // 상태를 바꾸는 거는 순서랑 상관없이 동시에 그려요.
+//                state.profileFlow = nil
+//                state.loginProviders = LoginProvidersFeature.State()
+//                // 2) 나중에 여기서 "로그인 화면 push"트리거 만들기
+//                return .send(.delegate(.shouldNavigateToLogin))
+                
             case .profileFlow(.presented(.loginButtonTapped)):
-                // 1) 로그인 안내 시트 닫기
-                state.profileFlow = nil
-                state.loginProviders = LoginProvidersFeature.State()
-                // 2) 나중에 여기서 "로그인 화면 push"트리거 만들기
-                return .none
+                return .send(.delegate(.loginButtonTapped))
+                
+
                 
             case .profileFlow(.presented(.cancelButtonTapped)):
                 state.profileFlow = nil
@@ -55,6 +75,10 @@ struct MainFeature {
             case .loginProviders:
                 // 나중에 실제 로그인 성공/실패 처리 추가 예정
                 return .none
+                
+            case .delegate:
+                return .none //멈추는것, Scope씌어서 관여를 안한다. <
+                
             }
         }
         .ifLet(\.$profileFlow, action: \.profileFlow) {
