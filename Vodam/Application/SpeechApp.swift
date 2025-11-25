c6159f3a151943b6005b3ad54f9f6b604ae059fb
Song Young Min <iasdsr1347@naver.com>
2025-11-25 10:51:11 +0900

    [FEAT] 구글 로그인 구현

--- Vodam/Application/SpeechApp.swift
+++ Vodam/Application/SpeechApp.swift
@@ -1,20 +1,27 @@
-import KakaoSDKCommon
-import KakaoSDKAuth
-import SwiftUI
 import ComposableArchitecture
+import GoogleSignIn
+import KakaoSDKAuth
+import KakaoSDKCommon
 import SwiftData
+import SwiftUI
 
 @main
 struct VodamApp: App {
-    
+
     init() {
-        guard let token = Bundle.main.object(forInfoDictionaryKey: "KAKAO_APP_KEY") as? String,
-                      !token.isEmpty else {
-                    fatalError("KAKAO_APP_KEY이 Info.plist에 설정되지 않았습니다. Secrets.xcconfig의 TOKEN 값을 Info.plist에 추가해주세요.")
-                }
-                KakaoSDK.initSDK(appKey: token)
+        guard
+            let token = Bundle.main.object(
+                forInfoDictionaryKey: "KAKAO_APP_KEY"
+            ) as? String,
+            !token.isEmpty
+        else {
+            fatalError(
+                "KAKAO_APP_KEY이 Info.plist에 설정되지 않았습니다. Secrets.xcconfig의 TOKEN 값을 Info.plist에 추가해주세요."
+            )
+        }
+        KakaoSDK.initSDK(appKey: token)
     }
-    
+
     var body: some Scene {
         WindowGroup {
             AppView(
@@ -25,12 +32,15 @@ struct VodamApp: App {
                     }
                 )
             )
-            
-            .onOpenURL{ url in
+
+            .onOpenURL { url in
                 if AuthApi.isKakaoTalkLoginUrl(url) {
                     _ = AuthController.handleOpenUrl(url: url)
                 }
+
+                _ = GIDSignIn.sharedInstance.handle(url)
             }
+
         }
         .modelContainer(for: [RecordingModel.self])
     }
