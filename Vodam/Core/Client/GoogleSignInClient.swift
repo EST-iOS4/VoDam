2e6a94951266ea80e6614a9496f502dd27df661f
Song Young Min <iasdsr1347@naver.com>
2025-11-25 11:11:47 +0900

    [FEAT] GoogleSignInClient 의존성 추가

--- Vodam/Core/Client/GoogleSignInClient.swift
+++ Vodam/Core/Client/GoogleSignInClient.swift
@@ -9,14 +9,39 @@ import Dependencies
 import GoogleSignIn
 
 struct GoogleSignInClient {
-    
+    var signOut: @Sendable () -> Void
+    var disconnect: @Sendable () async throws -> Void
     
 }
 
 extension GoogleSignInClient: DependencyKey {
+    static var liveValue: GoogleSignInClient {
+        .init (
+            signOut: {
+                GIDSignIn.sharedInstance.signOut()
+            },
+            disconnect: {
+                try await GIDSignIn.sharedInstance.disconnect()
+            }
+        )
+    }
+    
+    static var testValue: GoogleSignInClient {
+        .init(signOut: {}, disconnect: {})
+    }
+    
+    static var previewValue: GoogleSignInClient {
+        .testValue
+    }
     
 }
 
 extension DependencyValues {
-    
+    var googleSignInClient: GoogleSignInClient {
+        get {
+            self[GoogleSignInClient.self]
+        } set {
+            self[GoogleSignInClient.self] = newValue
+        }
+    }
 }
