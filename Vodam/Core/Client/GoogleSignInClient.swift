8f7eabf2321a124481a093148c9d49258fa92c27
Song Young Min <iasdsr1347@naver.com>
2025-11-25 11:06:37 +0900

    [ADD] GoogleSignInClient.swift 추가

---
+++ Vodam/Core/Client/GoogleSignInClient.swift
@@ -0,0 +1,22 @@
+//
+//  GoogleSignInClient.swift
+//  Vodam
+//
+//  Created by 송영민 on 11/25/25.
+//
+
+import Dependencies
+import GoogleSignIn
+
+struct GoogleSignInClient {
+    
+    
+}
+
+extension GoogleSignInClient: DependencyKey {
+    
+}
+
+extension DependencyValues {
+    
+}
