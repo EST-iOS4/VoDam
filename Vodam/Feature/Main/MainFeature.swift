b8f055b32c30fcd8498c81103f9da90dcf2ed1b8
Song Young Min <iasdsr1347@naver.com>
2025-11-28 14:29:31 +0900

    [CHORE] 저장된 프로젝트 갱신 수정

--- Vodam/Feature/Main/MainFeature.swift
+++ Vodam/Feature/Main/MainFeature.swift
@@ -91,10 +91,12 @@ struct MainFeature {
                 .presented(.delegate(.loggedOut(let isSuccess)))
             ):
                 if isSuccess {
+                    let ownerId = state.currentUser?.ownerId
                     state.currentUser = nil
                     return .run { _ in
                         await userStorageClient.clear()
                     }
+                    // 재로그인 시 데이터를 다시 볼 수 있도록 유지
                 }
                 return .none
                 
@@ -129,7 +131,7 @@ struct MainFeature {
                 state.profileFlow = nil
                 return .none
                 
-            // 통합 로그인 (카카오/애플/구글 통합)
+                // 통합 로그인 (카카오/애플/구글 통합)
             case .loginProviders(
                 .presented(.delegate(.login(let isSuccess, let user)))
             ):
@@ -150,8 +152,7 @@ struct MainFeature {
                 state.settings = nil
                 state.loginProviders = LoginProvidersFeature.State()
                 return .none
-       
-            // RecordingFeature delegate 처리
+                
             case .recording(.delegate(let delegateAction)):
                 switch delegateAction {
                 case .projectSaved(let projectId):
@@ -159,15 +160,13 @@ struct MainFeature {
                 case .syncCompleted(let projectId):
                     return .send(.delegate(.syncCompleted(projectId)))
                 }
-            
-            // FileButtonFeature delegate 처리
+                
             case .fileButton(.delegate(let delegateAction)):
                 switch delegateAction {
                 case .projectSaved(let projectId):
                     return .send(.delegate(.projectSaved(projectId)))
                 }
-            
-            // PDFButtonFeature delegate 처리
+                
             case .pdfButton(.delegate(let delegateAction)):
                 switch delegateAction {
                 case .projectSaved(let projectId):
