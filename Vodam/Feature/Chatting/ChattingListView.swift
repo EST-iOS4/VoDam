0d0fd4bcfe6afc1d9fbc12c4511e24630f99c7cd
Song Young Min <iasdsr1347@naver.com>
2025-12-03 14:53:05 +0900

    [FEAT] 채팅리스트 + 룸 firebase 삭제 추가

--- Vodam/Feature/Chatting/ChattingListView.swift
+++ Vodam/Feature/Chatting/ChattingListView.swift
@@ -34,18 +34,23 @@ struct ChattingListView: View {
     }
     
     private var ChattingListView: some View {
-        List(store.chattingList) { chattingInfo in
-            Button {
-                store.send(.chattingTapped(chattingInfo))
-            } label: {
-                ChattingItemView(chattingInfo: chattingInfo)
-                    .listRowSeparator(.hidden)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 12)
-                            .stroke(Color.secondary.opacity(0.6), lineWidth: 1)
-                    )
+        List {
+            ForEach(store.chattingList) { chattingInfo in
+                Button {
+                    store.send(.chattingTapped(chattingInfo))
+                } label: {
+                    ChattingItemView(chattingInfo: chattingInfo)
+                        .listRowSeparator(.hidden)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(Color.secondary.opacity(0.6), lineWidth: 1)
+                        )
+                }
+                .buttonStyle(.plain)
             }
-            .buttonStyle(.plain)
+            .onDelete { indexSet in
+                store.send(.delete(indexSet))
+            }
         }
         .listStyle(.plain)
         .scrollContentBackground(.hidden)
